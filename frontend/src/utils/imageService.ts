da892c25505e8be88be4fc2f9684f3759f58b2cc
shaniKupiec <shani.kupiec@example.com>
2025-06-26 23:25:44 +0300

    add default img

--- frontend/src/utils/imageService.ts
+++ frontend/src/utils/imageService.ts
@@ -1,5 +1,5 @@
 export const getPexelsImageUrl = (id: number | null | undefined) => {//width: number, height: number
-    if(!id) return undefined
+    if(!id) return 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM='
     const width = 500;
     const height = 500;
     return `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&w=${width}&h=${height}`
