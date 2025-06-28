927fb13ccac93331601fb70f73d3aa9333949395
shaniKupiec <shani.kupiec@example.com>
2025-06-28 13:14:44 +0300

    add note

--- frontend/src/utils/imageService.ts
+++ frontend/src/utils/imageService.ts
@@ -1,7 +1,8 @@
-export const getPexelsImageUrl = (id: number | null | undefined) => {//width: number, height: number
-    if(!id) return 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM='
-    const width = 500;
-    const height = 500;
+const width = 500;
+const height = 500;
+
+export const getPexelsImageUrl = (id: number | null | undefined) => {
+    // if there is no img return default img
+    if (!id) return 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM='
     return `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&w=${width}&h=${height}`
 }
-  
